98b431f184c9de11e9c5f517336a9cdf49794f6a
田中敏之 <gakitherock19841102@gmail.com>
2019-01-28 22:13:57 +0900

    訂正後
    内容被ってたのを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get '/users/new', to: 'users#new'
-
-  post '/users', to: 'users#create'
-  
   get '/users', to: 'users#index' 
   get '/users/new', to: 'users#new'
   post '/users', to: 'users#create'
