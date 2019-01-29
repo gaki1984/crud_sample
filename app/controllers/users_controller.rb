42b60df18eb0f75cee40e1553d433f26077a14bd
田中敏之 <gakitherock19841102@gmail.com>
2019-01-30 00:23:08 +0900

    CRUD処理の実装
    実装完了？

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def index
     @users = User.all
   end
@@ -9,6 +10,23 @@ class UsersController < ApplicationController
 
   def create
     User.create(user_params)
+    redirect_to :action => "index"
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to :action => "index"
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+    redirect_to :action => "index"
   end
 
   private
